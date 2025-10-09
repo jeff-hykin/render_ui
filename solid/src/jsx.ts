d722c49394de82e63763a7e9cc5959eac60764a6
Jeff Hykin <jeff.hykin@gmail.com>
2025-10-09 18:00:49 -0500

    -

--- solid/src/jsx.ts
+++ solid/src/jsx.ts
@@ -1,7 +1,7 @@
 import h from "https://esm.sh/solid-js@1.9.9/h?dev"
 
 export { h }
-// for JSX
-globalThis.React = {
-    createElement: h,
-}
+// // for JSX
+// globalThis.React = {
+//     createElement: h,
+// }
