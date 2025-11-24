250a03610ecb99967cab756448eaca6aa370b2e7
Jeff Hykin <jeff.hykin@gmail.com>
2025-11-24 17:49:21 -0600

    fixup, structure now working as intended

--- main/backendApi.js
+++ main/backendApi.js
@@ -7,11 +7,14 @@ import { escapeHtml } from 'https://esm.sh/gh/jeff-hykin/good-js@1.18.2.0/source
 import { FileSystem, glob } from "https://deno.land/x/quickr@0.8.6/main/file_system.js"
 import { jsBundle } from "./jsBundle.js"
 import { escapeJsString } from 'https://esm.sh/gh/jeff-hykin/good-js@1.18.2.0/source/flattened/escape_js_string.js'
-import bundledJs from "./frontend/main.ignore.js"
-import { hashItem } from "https://deno.land/x/quickr@9602671/main/flattened/hash_item.js"
+import bundledJs from "./frontend/main.bundle.js"
+import { hashItem } from "https://raw.githubusercontent.com/jeff-hykin/quickr/96026715afcc72bb185fee7a1d9c9a6875064f33/main/flat/hash_item.js"
 import storageObject from "https://deno.land/x/storage_object@0.0.2.0/main.js"
 import { certFileContents, keyFileContents } from "./dummyCertFiles.js"
 
+function escapeInnerJs(code) {
+    return code.replace("</script>", "<\\/script>")
+}
 const thisFolder = FileSystem.thisFolder
 const defaultPort = 4321
 export function relayMessages(config={}) {
@@ -46,7 +49,7 @@ export function relayMessages(config={}) {
             //   console.log(`Running on http://127.0.0.1:9093`)
             // },
         },
-        (req) => {
+        async (req) => {
             const url = new URL(req.url)
             //
             // asked for something other than websocket
@@ -61,12 +64,15 @@ export function relayMessages(config={}) {
                     if (hash != storageObject.renderUiBundleHash) {
                         storageObject.renderUiBundleHash = hash
                         console.log(`bundling frontend ... `)
-                        code = await jsBundle(`${thisFolder}/frontend/main.js`)
-                        console.log(`bundled`)
-                        // save the bundle, escaped for html, for static importing later
-                        FileSystem.writeFile(`${thisFolder}/frontend/main.ignore.js`, `export default ${escapeJsString(escapeHtml(code))}`).catch((error)=>{
-                            console.warn(`failed to save the bundled frontend, nbd: ${error}`)
-                        })
+                        try {
+                            code = escapeInnerJs(new TextDecoder().decode(await jsBundle(`${thisFolder}/frontend/main.js`)))
+                            // save the bundle, escaped for html, for static importing later
+                            FileSystem.write({path:`${thisFolder}/frontend/main.bundle.js`, data:`export default ${escapeJsString(code)}`}).catch((error)=>{
+                                console.warn(`failed to save the bundled frontend, nbd: ${error}`)
+                            })
+                        } catch (error) {
+                            console.log(`failed to bundle frontend: ${error}`)
+                        }
                     }
                 }
                 return new Response(new TextEncoder().encode(`<!DOCTYPE html>
@@ -75,17 +81,292 @@ export function relayMessages(config={}) {
                         <meta charset="UTF-8">
                         <meta name="viewport" content="width=device-width, initial-scale=1.0">
                         <title>${escapeHtml(config.title||"Render UI")}</title>
-                        <script type="module">
-                            import { renderUiConnectionKey } from "https://esm.sh/gh/jeff-hykin/render_ui@cae2868/main/frontend/connectionsCore.js"
-                            globalThis[renderUiConnectionKey].connections = {
-                                default: ${escapeHtml(JSON.stringify(connectionInfo))},
+                        <style>
+                            @-ms-viewport {
+                              width: device-width;
                             }
-                        </script>
-                        <script type="module">
-                            ${code /* NOTE: this has already been escaped for html (e.g. cached escape) */}
-                        </script>
+                            article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section, main, summary {
+                              display: block;
+                            }
+
+                            *, *::before, *::after {
+                              box-sizing: inherit;
+                            }
+
+                            html {
+                              /* 1 */
+                              box-sizing: border-box;
+                              /* 2 */
+                              touch-action: manipulation;
+                              /* 3 */
+                              -webkit-text-size-adjust: 100%;
+                              -ms-text-size-adjust: 100%;
+                              /* 4 */
+                              -ms-overflow-style: scrollbar;
+                              /* 5 */
+                              -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+                            }
+
+                            body {
+                              line-height: 1;
+                            }
+
+                            html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video, main {
+                              font-size: 100%;
+                              font: inherit;
+                              vertical-align: baseline;
+                            }
+
+                            ol, ul {
+                              list-style: none;
+                            }
+
+                            blockquote, q {
+                              quotes: none;
+                            }
+
+                            blockquote::before, blockquote::after, q::before, q::after {
+                              content: "";
+                              content: none;
+                            }
+
+                            table {
+                              border-collapse: collapse;
+                              border-spacing: 0;
+                            }
+
+                            hr {
+                              /* 1 */
+                              box-sizing: content-box;
+                              height: 0;
+                              /* 2 */
+                              overflow: visible;
+                            }
+
+                            pre, code, kbd, samp {
+                              /* 1 */
+                              font-family: monospace, monospace;
+                            }
+
+                            pre {
+                              /* 2 */
+                              overflow: auto;
+                              /* 3 */
+                              -ms-overflow-style: scrollbar;
+                            }
+
+                            a {
+                              /* 1 */
+                              background-color: transparent;
+                              /* 2 */
+                              -webkit-text-decoration-skip: objects;
+                            }
+
+                            abbr[title] {
+                              /* 1 */
+                              border-bottom: none;
+                              /* 2 */
+                              text-decoration: underline;
+                              text-decoration: underline dotted;
+                            }
+
+                            b, strong {
+                              font-weight: bolder;
+                            }
+
+                            small {
+                              font-size: 80%;
+                            }
+
+                            sub, sup {
+                              font-size: 75%;
+                              line-height: 0;
+                              position: relative;
+                            }
+
+                            sub {
+                              bottom: -0.25em;
+                            }
+
+                            sup {
+                              top: -0.5em;
+                            }
+
+                            img {
+                              border-style: none;
+                            }
+
+                            svg:not(:root) {
+                              overflow: hidden;
+                            }
+
+                            button {
+                              border-radius: 0;
+                            }
+
+                            input, button, select, optgroup, textarea {
+                              font-family: inherit;
+                              font-size: inherit;
+                              line-height: inherit;
+                            }
+
+                            button, [type=reset], [type=submit], html [type=button] {
+                              -webkit-appearance: button;
+                            }
+
+                            input[type=date], input[type=time], input[type=datetime-local], input[type=month] {
+                              -webkit-appearance: listbox;
+                            }
+
+                            fieldset {
+                              min-width: 0;
+                            }
+
+                            [tabindex="-1"]:focus {
+                              outline: 0 !important;
+                            }
+
+                            button, input {
+                              overflow: visible;
+                            }
+
+                            button, select {
+                              text-transform: none;
+                            }
+
+                            button::-moz-focus-inner, [type=button]::-moz-focus-inner, [type=reset]::-moz-focus-inner, [type=submit]::-moz-focus-inner {
+                              border-style: none;
+                              padding: 0;
+                            }
+
+                            legend {
+                              /* 1 */
+                              max-width: 100%;
+                              white-space: normal;
+                              /* 2 */
+                              color: inherit;
+                              /* 3 */
+                              display: block;
+                            }
+
+                            progress {
+                              vertical-align: baseline;
+                            }
+
+                            textarea {
+                              overflow: auto;
+                            }
+
+                            [type=checkbox], [type=radio] {
+                              /* 1 */
+                              box-sizing: border-box;
+                              /* 2 */
+                              padding: 0;
+                            }
+
+                            [type=number]::-webkit-inner-spin-button, [type=number]::-webkit-outer-spin-button {
+                              height: auto;
+                            }
+
+                            [type=search] {
+                              /* 1 */
+                              -webkit-appearance: textfield;
+                              /* 2 */
+                              outline-offset: -2px;
+                            }
+
+                            [type=search]::-webkit-search-cancel-button, [type=search]::-webkit-search-decoration {
+                              -webkit-appearance: none;
+                            }
+
+                            ::-webkit-file-upload-button {
+                              /* 1 */
+                              -webkit-appearance: button;
+                              /* 2 */
+                              font: inherit;
+                            }
+
+                            template {
+                              display: none;
+                            }
+
+                            [hidden] {
+                              display: none;
+                            }
+
+                            button:focus {
+                              outline: 1px dotted;
+                              outline: 5px auto -webkit-focus-ring-color;
+                            }
+
+                            button:-moz-focusring, [type=button]:-moz-focusring, [type=reset]:-moz-focusring, [type=submit]:-moz-focusring {
+                              outline: 1px dotted ButtonText;
+                            }
+
+                            html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video, main {
+                              margin: 0;
+                              padding: 0;
+                              border: 0;
+                            }
+
+                            input, button, select, optgroup, textarea {
+                              margin: 0;
+                            }
+
+                            body {
+                              width: 100vw;
+                              min-height: 100vh;
+                              overflow: visible;
+                              scroll-behavior: auto;
+                            }
+
+                            textarea {
+                              resize: vertical;
+                            }
+
+                            br {
+                              display: inline-block;
+                              content: "";
+                              border-bottom: 0px solid transparent;
+                            }
+                        </style>
                     </head>
-                </html>`), { status: 200, headers: { "content-type": "text/plain" } })
+                        <body>
+                            <div style="display: flex;justify-content: center;align-items: center;height: 100vh;">
+                                <div style="width: 50px;height: 50px;border: 10px solid #dddddd;border-top-color: #009579;border-radius: 50%;transform: rotate(0.16turn);" id="good-component--initial-loader">
+                                </div>
+                            </div>
+                        </body>
+                        <script>
+                            // 
+                            // Synchonous/Fast loading animation
+                            // 
+                                const animateLoader = ()=>{
+                                const element = document.getElementById("good-component--initial-loader")
+                                    element && element.animate(
+                                        [
+                                            { transform: 'rotate(0turn)' },
+                                            { transform: 'rotate(1turn)' },
+                                        ],
+                                        {
+                                            duration: 1000,
+                                            iterations: Infinity,
+                                            easing: 'ease',
+                                        },
+                                    )
+                                }
+                                document.body ? animateLoader() : document.addEventListener("DOMContentLoaded", animateLoader)
+                        </script>
+                    <script type="module">
+                        import { renderUiConnectionKey } from "https://esm.sh/gh/jeff-hykin/render_ui@cae2868/main/frontend/connectionsCore.js"
+                        globalThis[renderUiConnectionKey].connections = {
+                            default: ${escapeInnerJs(JSON.stringify(connectionInfo))},
+                        }
+                    </script>
+                    <script type="module">
+                        ${code /* NOTE: this has already been escaped for html (e.g. cached escape) */}
+                    </script>
+                </html>`), { status: 200, headers: { "content-type": "text/html" } })
             // 
             // socket
             // 
--- main/frontend/main.bundle.js
+++ main/frontend/main.bundle.js
@@ -1,4 +1,4 @@
-import { createEditor } from 'https://cdn.jsdelivr.net/gh/jeff-hykin/codemirror_esm@1.0.0.5/helpers.js'
+export default `import { createEditor } from 'https://cdn.jsdelivr.net/gh/jeff-hykin/codemirror_esm@1.0.0.5/helpers.js'
 import { javascript } from 'https://cdn.jsdelivr.net/gh/jeff-hykin/codemirror_esm@1.0.0.5/@codemirror/lang-javascript.js'
 import { bash } from 'https://cdn.jsdelivr.net/gh/jeff-hykin/codemirror_esm@1.0.0.5/@codemirror/lang-bash.js'
 import throttle from 'https://esm.sh/lodash/throttle'
@@ -6,31 +6,32 @@ import { translate } from 'https://esm.sh/gh/jeff-hykin/bash2deno@master/api.js'
 import { renderUiConnectionKey } from "https://esm.sh/gh/jeff-hykin/render_ui@cae2868/main/frontend/connectionsCore.js"
 var { html, passAlongProps } = await import("https://esm.sh/gh/jeff-hykin/elemental@0.6.5/main/deno.js?dev")
 
+
 const connections = globalThis[renderUiConnectionKey].connections
 // DEBUGGING ONLY
 globalThis.connections = connections
-console.debug(`connections is:`,connections)
+console.debug(\`connections is:\`,connections)
 
 function connect(connectionName) {
     const connection = connections[connectionName]
     if (connection) {
         const { hostname, port } = connection
-        console.log(`connecting to ${hostname}:${port}`)
-        return new WebSocket(`ws://${hostname}:${port}/render_ui_listener/${connectionName}`)
+        console.log(\`connecting to \${hostname}:\${port}\`)
+        return new WebSocket(\`ws://\${hostname}:\${port}/render_ui_listener/\${connectionName}\`)
     }
 }
 globalThis.connect = connect
 
 
 
-const warningsDiv = html`<div style=${`
+const warningsDiv = html\`<div style=\${\`
     height: 12rem;
     position: fixed;
     left: 0;
     bottom: 0;
     background-color: gray;
     overflow-y: scroll;
-`}/>`
+\`}/>\`
 
 const javascriptEditor = createEditor({
     style: "width:50vw;height:100vh",
@@ -39,41 +40,41 @@ const javascriptEditor = createEditor({
 
 const convertCode = throttle(({element, editor}, update)=>{
     try {
-        console.debug(`element.value is:`,element.value)
+        console.debug(\`element.value is:\`,element.value)
         let { jsCode, xmlStylePreview } = translate(element.value)
         // remove fs if its not needed (hack)
-        if (jsCode.match(/\bfs\b/g).length > 1) {
-            jsCode = jsCode.replace(/.+\bfs\b.+\n/g, "")
+        if (jsCode.match(/\\bfs\\b/g).length > 1) {
+            jsCode = jsCode.replace(/.+\\bfs\\b.+\\n/g, "")
         }
         // remove path if its not needed (hack)
-        if (jsCode.match(/\bpath\b/g).length > 1) {
-            jsCode = jsCode.replace(/.+\bpath\b.+\n/g, "")
+        if (jsCode.match(/\\bpath\\b/g).length > 1) {
+            jsCode = jsCode.replace(/.+\\bpath\\b.+\\n/g, "")
         }
         javascriptEditor.value = jsCode
-        // warningsDiv.innerHTML = warnings.map(each=>`<span>${each}</span>`.replace(/\n/g,"<br>")).join("<br>")
+        // warningsDiv.innerHTML = warnings.map(each=>\`<span>\${each}</span>\`.replace(/\\n/g,"<br>")).join("<br>")
     } catch (error) {
-        warningsDiv.innerHTML = error.message + "\n" + error.stack.replace(/\n/g,"<br>")
+        warningsDiv.innerHTML = error.message + "\\n" + error.stack.replace(/\\n/g,"<br>")
     }
 }, 1000)
 
 const bashEditor = createEditor({
     style: "width:50vw;height:100vh",
     language: bash,
-    value: `
+    value: \`
         echo "hello" > file.txt
-    `.replace(/\n                /g, "\n").slice(1),
+    \`.replace(/\\n                /g, "\\n").slice(1),
     onInput: convertCode,
 })
 
 // initial convert
 setTimeout(() => {
-    console.log(`initial convert`)
+    console.log(\`initial convert\`)
     convertCode({element: bashEditor})
 }, 500)
 
 
-document.body = html`<body display=flex>
-    ${bashEditor}
-    ${javascriptEditor}
-    ${warningsDiv}
-</body>`
+document.body = html\`<body display=flex>
+    \${bashEditor}
+    \${javascriptEditor}
+    \${warningsDiv}
+</body>\``
